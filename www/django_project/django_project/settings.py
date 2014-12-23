b9446483324e9a9ea5cc01856ec9bf956f041142
Adam LeBlanc <leblanca37@gmail.com>
2014-12-23 13:39:52 -0400

    This is the initial commit that has an empty template for the website
    - www: The root directory of the website
    - requirements .txt: The requirements for the project
        go pip install -r requirements .txt to install all requirements into your virutalenv
    - django_project: the django_project
    
    TODO: Create a static directory for serving static files, add instructions to the README for developers to run the
    project locally
    
    Note: before you make your commits make sure there is nothing staged that isn't source code or resources. Example:
    We do not want files that your IDE creates (Eclipse, Sublime, Emacs, or other). Before you make your commit run
    git status to make sure. If there is a file like that, upstage those files, and add them to the gitignore file.

---
+++ www/django_project/django_project/settings.py
@@ -0,0 +1,97 @@
+"""
+Django settings for django_project project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'w9(rr5@687akit39h3m69osj*m5+gn0((9kfcgn7t&67tl9wm9'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+# apps that are installed by deault
+DEFAULT_APPS =  (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+# Apps we did not code
+THIRD_PARTY_APPS = ()
+
+# Apps we did code
+FIRST_PARTY_APPS = ()
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + FIRST_PARTY_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'django_project.urls'
+
+WSGI_APPLICATION = 'django_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'beagle_db',
+        # Addjust as need for local development, but change back before pushing back to github
+        # also remember to set to the proper user, and password on the actual server
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'HOST' : 'localhost',
+        'PORT' : '3360'
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-ca'
+
+TIME_ZONE = 'UTC-04:00'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
